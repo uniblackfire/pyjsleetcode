3bf52767b2ef9540a6be40219c5aff7c761c9a23
uniblackfire <uniblackfire@gmail.com>
2017-07-20 20:36:41 +0800

    Reverse Vowels of a String

--- Reverse Vowels of a String.js
+++ Reverse Vowels of a String.js
@@ -14,6 +14,30 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
+var reverseVowels = function (s) {
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
 
+    let s_arr = Array.from(s);
+    let i = 0;
+    let j = s_arr.length - 1;
+    while (i < j) {
+        while (i < j && !vowels.has(s_arr[i])) {
+            i += 1;
+        }
+        while (i < j && !vowels.has(s_arr[j])) {
+            j -= 1;
+        }
+
+        let tmp_ch = s_arr[i];
+        s_arr[i] = s_arr[j];
+        s_arr[j] = tmp_ch;
+
+        console.log(i, j);
+        console.log(s_arr);
+        i += 1;
+        j -= 1;
+    }
+    return s_arr.join('');
 };
+
+console.log(reverseVowels('leetcode'));
