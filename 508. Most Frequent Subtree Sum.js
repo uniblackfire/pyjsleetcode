120e579e0ef5007c7a8459a96928c3c0c0716600
uniblackfire <uniblackfire@gmail.com>
2017-09-10 12:25:39 +0800

    findFrequentTreeSum

---
+++ 508. Most Frequent Subtree Sum.js
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/most-frequent-subtree-sum/description/
+// Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
+//
+// Examples 1
+// Input:
+//
+//   5
+//  /  \
+// 2   -3
+// return [2, -3, 4], since all the values happen only once, return all of them in any order.
+// Examples 2
+// Input:
+//
+//   5
+//  /  \
+// 2   -5
+// return [2], since 2 happens twice, however -5 only occur once.
+// Note: You may assume the sum of values in any subtree is in the range of 32-bit signed integer.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findFrequentTreeSum = function (root) {
+    function dfs(root) {
+        if (!root)
+            return 0;
+        let left = dfs(root.left);
+        let right = dfs(root.right);
+        let sum = root.val + left + right;
+        let count = map.has(sum) ? map.get(sum) + 1 : 1;
+        map.set(sum, count);
+        if (count > maxCount) {
+            result = [sum];
+            maxCount = count;
+        } else if (count === maxCount) {
+            result.push(sum);
+        }
+        return sum;
+    }
+
+    let result = [];
+    let maxCount = -1;
+    let map = new Map();
+    dfs(root, map);
+    return result;
+};
