eed34beadcb287ee6f36569a2004cb8a30c987a3
uniblackfire <uniblackfire@gmail.com>
2017-09-11 07:36:12 +0800

    getRandom

--- Queue Reconstruction by Height.js
+++ Queue Reconstruction by Height.js
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/queue-reconstruction-by-height/description/
 //
-// Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+// Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (head, k), where head is the height of the person and k is the number of people in front of this person who have a height greater than or equal to head. Write an algorithm to reconstruct the queue.
 //
 // Note:
 // The number of people is less than 1,100.
