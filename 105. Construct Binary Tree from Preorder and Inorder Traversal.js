b305ce8c09dbc95bbd006b2dc6a14ec28b6575f7
uniblackfire <uniblackfire@gmail.com>
2017-09-19 22:07:03 +0800

    create bt

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.js
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -19,6 +19,13 @@
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
+    // Our aim is to find out the index of right child for current node in the preorder array
+// We know the index of current node in the preorder array - preStart (or whatever your call it), it's the root of a subtree
+// Remember pre order traversal always visit all the node on left branch before going to the right ( root -> left -> ... -> right), therefore, we can get the immediate right child index by skipping all the node on the left branches/subtrees of current node
+// The inorder array has this information exactly. Remember when we found the root in "inorder" array we immediately know how many nodes are on the left subtree and how many are on the right subtree
+// Therefore the immediate right child index is preStart + numsOnLeft + 1 (remember in preorder traversal array root is always ahead of children nodes but you don't know which one is the left child which one is the right, and this is why we need inorder array)
+// numsOnLeft = root - inStart.
+
     function recursion(preorder, preStart, preEnd, inorder, inStart, inEnd, inMap) {
         if (preStart > preEnd || inStart > inEnd) return null;
 
@@ -96,9 +103,3 @@ var buildTree = function (preorder, inorder) {
     }
     return root;
 };
-// Our aim is to find out the index of right child for current node in the preorder array
-// We know the index of current node in the preorder array - preStart (or whatever your call it), it's the root of a subtree
-// Remember pre order traversal always visit all the node on left branch before going to the right ( root -> left -> ... -> right), therefore, we can get the immediate right child index by skipping all the node on the left branches/subtrees of current node
-// The inorder array has this information exactly. Remember when we found the root in "inorder" array we immediately know how many nodes are on the left subtree and how many are on the right subtree
-// Therefore the immediate right child index is preStart + numsOnLeft + 1 (remember in preorder traversal array root is always ahead of children nodes but you don't know which one is the left child which one is the right, and this is why we need inorder array)
-// numsOnLeft = root - inStart.
