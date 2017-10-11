40044f23dde7464fbaac6860baf032b43ac1d202
ljp <ljp@MAXMAXMAX.local>
2017-10-11 11:43:44 +0800

    313. super ugly number

--- 313. Super Ugly Number.js
+++ 313. Super Ugly Number.js
@@ -3,10 +3,10 @@
 
 // Write a program to find the nth super ugly number.
 //
-//     Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
+// Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
 //
 // Note:
-//     (1) 1 is a super ugly number for any given primes.
+// (1) 1 is a super ugly number for any given primes.
 // (2) The given numbers in primes are in ascending order.
 // (3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.
 // (4) The nth super ugly number is guaranteed to fit in a 32-bit signed integer.
@@ -111,8 +111,8 @@ var nthSuperUglyNumber3 = function (n, primes) {
 
     let pq = new PriorityQueue((item1, item2) => item1[0] < item2[0]);
 
-    const result = new Array(n);
-    result[0] = 1;
+    const ugly = new Array(n);
+    ugly[0] = 1;
 
     for (let i = 0; i < primes.length; i++) {
         // n[0] current value, n[1] current index, n[2] base value
@@ -121,15 +121,15 @@ var nthSuperUglyNumber3 = function (n, primes) {
 
     for (let i = 1; i < n; i++) {
         let next = pq.peek()[0];
-        result[i] = next;
+        ugly[i] = next;
         while (pq.peek()[0] === next) {
-            let cur = pq.poll();
-            cur[0] = cur[2] * result[cur[1]];
-            cur[1] = cur[1] + 1;
-            pq.offer(cur);
+            let pqItem = pq.poll();
+            pqItem[0] = pqItem[2] * ugly[pqItem[1]];
+            pqItem[1] += 1;
+            pq.offer(pqItem);
         }
     }
 
-    return result[n - 1];
+    return ugly[n - 1];
 };
 console.log(nthSuperUglyNumber3(4, [2, 7, 13, 19]));
