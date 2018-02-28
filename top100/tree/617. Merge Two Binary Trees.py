0f351348aba0592ca2cd84c4eb156d04a04935fb
uniblackfire <uniblackfire@gmail.com>
2018-02-28 17:17:24 +0800

    fff

--- top100/tree/617. Merge Two Binary Trees.py
+++ top100/tree/617. Merge Two Binary Trees.py
@@ -32,11 +32,7 @@ class Solution:
         """
         if not t1 and not t2:
             return None
-        val = 0
-        if t1:
-            val = val + t1.val
-        if t2:
-            val = val + t2.val
+        val = (t1.val if t1 else 0) + (t2.val if t2 else 0)
         new_node = TreeNode(val)
         new_node.left = self.mergeTrees(t1.left if t1 else None, t2.left if t2 else None)
         new_node.right = self.mergeTrees(t1.right if t1 else None, t2.right if t2 else None)
