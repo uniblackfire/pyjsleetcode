5cabba500093ce31ae50183e371837c52710ae5f
uniblackfire <uniblackfire@gmail.com>
2018-02-28 17:21:54 +0800

    add new sol

--- top100/tree/617. Merge Two Binary Trees.py
+++ top100/tree/617. Merge Two Binary Trees.py
@@ -22,6 +22,7 @@
 # 	 5   4   7
 # Note: The merging process must start from the root nodes of both trees.
 
+import collections
 
 class Solution:
     def mergeTrees(self, t1, t2):
@@ -37,3 +38,21 @@ class Solution:
         new_node.left = self.mergeTrees(t1.left if t1 else None, t2.left if t2 else None)
         new_node.right = self.mergeTrees(t1.right if t1 else None, t2.right if t2 else None)
         return new_node
+    
+    def mergeTrees2(self, t1, t2):
+        if not (t1 and t2):
+            return t1 or t2
+        queue1, queue2 = collections.deque([t1]), collections.deque([t2])
+        while queue1 and queue2:
+            node1, node2 = queue1.popleft(), queue2.popleft()
+            if node1 and node2:
+                node1.val = node1.val + node2.val
+                if (not node1.left) and node2.left:
+                    node1.left = TreeNode(0)
+                if (not node1.right) and node2.right:
+                    node1.right = TreeNode(0)
+                queue1.append(node1.left)
+                queue1.append(node1.right)
+                queue2.append(node2.left)
+                queue2.append(node2.right)
+        return t1
